9a197c84f3375b51a61657ec842e45be5e6439b2
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2023-04-15 12:58:10 -0400

    added generi login

--- src/test/java/tests/AppTest2.java
+++ src/test/java/tests/AppTest2.java
@@ -11,7 +11,26 @@ public class AppTest2 extends BaseTest {
     super(DEMO_2);
   }
 
+  private By login(final String username,final  String password,final  String xpathToValidate) {
+    setText(By.xpath("//android.widget.EditText[@content-desc=\"test-Username\"]"),
+        username);
+    setText(By.xpath("//android.widget.EditText[@content-desc=\"test-Password\"]"), password);
+    //waitForSeconds(1);
+    driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"test-LOGIN\"]")).click();
+    waitForSeconds(2);
+    By elementToValidate = By.xpath(xpathToValidate);
+    waitForSeconds(1);
+    takeScreenshot("login");
+    return elementToValidate;
+  }
+  
   @Test
+  public void incorrectLogin() {
+    By message = login("fakeUser", "fakePassword", "//android.view.ViewGroup[@content-desc=\"test-Error message\"]/android.widget.TextView");
+    Assert.assertNotNull(message, "Login incorrect!");
+  }
+  
+  @Test(priority = 1)
   public void login() {
     setText(By.xpath("//android.widget.EditText[@content-desc=\"test-Username\"]"),
         "standard_user");
@@ -21,11 +40,12 @@ public class AppTest2 extends BaseTest {
     waitForSeconds(3);
     takeScreenshot("login");
     By landingViewProductsText = By.xpath("//android.view.ViewGroup[@content-desc=\"test-Cart drop zone\"]/android.view.ViewGroup/android.widget.TextView");
-    Assert.assertNotNull(landingViewProductsText, "Login incorrect!");
+    //By landingViewProductsText = login("standard_user","secret_sauces","//android.view.ViewGroup[@content-desc=\\\"test-Cart drop zone\\\"]/android.view.ViewGroup/android.widget.TextView");
+    Assert.assertNotNull(landingViewProductsText, "User successfully logged in!");
   }
   
   
-  @Test
+  @Test(priority = 2)
   public void products() {
     //driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"test-Cart drop zone\"]/android.view.ViewGroup/android.widget.TextView")).click();
     driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\"test-ADD TO CART\"])[1]/android.widget.TextView").click();
